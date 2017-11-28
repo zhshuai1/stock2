f365e2c56708da4c10b0eedb06cf5655c61ec4dd
zhshuai1 <zh_ang_ok@yeah.net>
2017-11-29 07:35:18 +0800

    add logic to retrieve us stocks

---
+++ update_us.js
@@ -0,0 +1,7 @@
+var Fs = require('fs');
+var Stock = require('./lib/common');
+var stocks = Fs.readFileSync('./stocks/us.stock', 'utf-8').split('\r\n');
+var poolSize = 20;
+var days = 10;
+var pool = Stock.pool('us', stocks, days, poolSize);
+pool.start();
