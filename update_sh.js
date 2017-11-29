85d053d16e2b41157bfc6ac52e7f997e1e8ea211
zhshuai1 <zh_ang_ok@yeah.net>
2017-11-29 09:44:35 +0800

    use pain text to store stock so that this could support multi language processing

--- update_sh.js
+++ update_sh.js
@@ -1,9 +1,8 @@
 var Stock = require('./lib/common');
-var sh = require('./stocks/sh');
-
+var stocks = Stock.fileReader().getStocks('./stocks/sh.stock');
 var poolSize = 20;
 var days = 10;
-var pool = Stock.pool('sh', sh.stocks, days, poolSize);
+var pool = Stock.pool('sh', stocks, days, poolSize);
 pool.start();
 process.on('uncaughtException', function (error) {
     console.log(error);
--- update_sz.js
+++ update_sz.js
@@ -1,9 +1,9 @@
 var Stock = require('./lib/common');
-var sz = require('./stocks/sz');
+var stocks = Stock.fileReader().getStocks('./stocks/sz.stock');
 
 var poolSize = 20;
 var days = 10;
-var pool = Stock.pool('sz', sz.stocks, days, poolSize);
+var pool = Stock.pool('sz', stocks, days, poolSize);
 pool.start();
 process.on('uncaughtException', function (error) {
     console.log(error);
